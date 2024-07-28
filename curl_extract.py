393cf5c1961ec2d005841421789db3ad32800cab
devops_enginar <ahmetkasimerbay@192.168.1.38>
2024-07-28 16:57:51 +0300

    filters updated

--- curl_extract.py
+++ curl_extract.py
@@ -2,8 +2,35 @@ from bs4 import BeautifulSoup
 import json
 import re
 
-# Load the HTML content from the file
-with open("bitbucket_permission_management.html", "r", encoding="utf-8") as file:
+def curl_filter(code_text):
+    # Clean the curl command
+    curl_command = code_text.replace('\n', ' ').replace('\\', '').strip()
+
+    # Replace http with https
+    curl_command = curl_command.replace('http://', 'https://')
+
+    # Replace unnecessary spaces 
+    curl_command = curl_command.replace("    ", " ")
+    curl_command = curl_command.replace(": ", ":")
+    curl_command = curl_command.replace("  ", "")
+    curl_command = curl_command.replace("{ ", "{").replace(" }", "}")
+    curl_command = curl_command.replace(", ", ",")
+    curl_command = curl_command.replace(" ]", "]")
+
+    # Replace unnecessary "\"
+    curl_command = curl_command.replace("\"\\", "\\\"")
+    # Remove 'none' from the URL
+    curl_command = re.sub(r'none', '', curl_command)
+
+    # Add "-n" as option
+    curl_command = curl_command.replace("GET ", "GET -n ")
+    curl_command = curl_command.replace("DELETE ", "DELETE -n ")
+    curl_command = curl_command.replace("POST ", "POST -n ")
+    curl_command = curl_command.replace("PUT ", "PUT -n ")
+
+    return curl_command
+
+with open(f"./htmls/bitbucket_permission_management.html", "r", encoding="utf-8") as file:
     content = file.read()
 
 # Parse the HTML content
@@ -15,32 +42,11 @@ curl_commands_with_params = []
 # Find all <code> elements that contain curl commands
 for code_tag in soup.find_all('code'):
     code_text = code_tag.get_text()
+
     if 'curl' in code_text:
-        # Clean the curl command
-        curl_command = code_text.replace('\n', ' ').replace('\\', '').strip()
-        
-        # Replace http with https
-        curl_command = curl_command.replace('http://', 'https://')
-        
-        # Remove any spaces after colon
-        curl_command = re.sub(r':\s+', ':', curl_command)
-        
-        # Remove 'none' from the URL
-        curl_command = re.sub(r'none', '', curl_command)
 
-        # Extract and remove --data sections
-        data_sections = {}
+        curl_command = curl_filter(code_text)
 
-        def extract_data(match):
-            key = '--data'
-            value = match.group(1).strip('"\'')
-            data_sections[key] = f'"{value}"'  # Enclose the value in double quotes
-            return ''  # Remove the --data part from the curl command
-        
-        # Adjust regex to match --data and its value (with or without quotes)
-        curl_command = re.sub(r'--data\s+(".*?"|\'.*?\'|\S+)', extract_data, curl_command)
-        
-        
         # Extract query parameters from the curl command if available
         query_params = {}
 
@@ -52,15 +58,11 @@ for code_tag in soup.find_all('code'):
                 if '=' in pair:
                     key, value = pair.split('=')
                     query_params[key] = value
-        
+
         # Create a dictionary for the curl command
         curl_command_entry = {
             'curl_command': curl_command
         }
-
-        # Add --data sections if they exist
-        if data_sections:
-            curl_command_entry['data_sections'] = data_sections
 
         # Add query parameters if they exist
         if query_params:
@@ -69,5 +71,6 @@ for code_tag in soup.find_all('code'):
         # Append the dictionary to the list
         curl_commands_with_params.append(curl_command_entry)
 
-# Output the extracted details
-print(json.dumps(curl_commands_with_params, indent=4))
+with open("curls.txt", "w") as f:
+    print(json.dumps(curl_commands_with_params, indent=4), file=f)
+
